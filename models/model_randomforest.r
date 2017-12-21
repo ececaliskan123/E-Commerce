eaf3dde974d3d0477e161ca8a17722e4b869c6bb
bluehyunah <34094908+bluehyunah@users.noreply.github.com>
2017-12-21 18:22:28 +0100

    Add files via upload

---
+++ models/model_randomforest.r
@@ -0,0 +1,36 @@
+#### Ece's data cleaning base
+## row 99 in Known.r file, retail$return2 <- retail$return 
+## I need 0,1 so I add row 99 because ece switched return class like retail$return <- ("keep","return")
+
+### Random forest
+library(ranger)
+library(dplyr)
+# From Parameter tuning file
+set.seed(124)
+n <- nrow(retail) 
+sample.size <- ceiling(n*0.8)
+idx.train <- createDataPartition(y =retail$return2, p = 0.75, list = FALSE) 
+tr <- retail[idx.train, ]  # training set
+ts <- retail[-idx.train, ] # test set 
+# The return column
+return <- "return2"
+# The input variables
+vars <- c("user_dob", "delivery_duration", "user_maturity",
+          "user_state", "item_price", "item_color", "item_size", "month_of_delivery")
+# Create the formula string for returns as a function of the inputs
+fmla <- paste(return, "~", paste(vars, collapse = " + "))
+# Fit and print the random forest model
+(retail_model_rf <- ranger(fmla, # formula 
+                           tr,   # data
+                           num.trees = 500, 
+                           respect.unordered.factors = "order"))
+# Make predictions on the ts data
+ts$pred <- predict(retail_model_rf, ts)$predictions
+# Calculate the RMSE of the predictions
+ts %>% 
+  mutate(residual = return2 - pred)  %>%        # calculate the residual
+  summarize(rmse  = sqrt(mean(residual^2)))    # calculate rmse
+# Plot actual outcome vs predictions (predictions on x-axis)
+ggplot(ts, aes(x = pred, y = return2)) + 
+  geom_point() + 
+  stat_smooth(method = 'glm', method.args = list(family = 'binomial'), se = FALSE) # Smoothed curve w/o standard errors
