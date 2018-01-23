0d536a2705e895a2a63fa5845a99d8ab583e4d3c
Felix Wolff <felix.wolff2@student.hpi.de>
2018-01-23 18:16:45 +0100

    Clean up

--- stacked_model.R
+++ stacked_model.R
@@ -1,4 +1,6 @@
 if(!require("mlr")) install.packages("mlr"); library("mlr")
+if(!require("caret")) install.packages("caret"); library("caret")
+
 #setwd("/mnt/learning/business-analytics-data-science/groupwork/")
 source('load_data.R')
 
@@ -11,6 +13,7 @@ rf_class = read.csv("data/randomforest_probability.csv", stringsAsFactors = FALS
 nnet_known = read.csv("data/nnet_known.csv", stringsAsFactors = FALSE)
 nnet_class = read.csv("data/nnet_class.csv", stringsAsFactors = FALSE)
 d = read_and_preprocess_data_file('data/BADS_WS1718_known.csv')
+c = read_and_preprocess_data_file('data/BADS_WS1718_class.csv')
 
 # sort everything first
 xgboost_known = xgboost_known[order(xgboost_known$order_item_id),]
@@ -35,27 +38,29 @@ stopifnot(known_labels_complete && class_labels_complete)
 df_known = data.frame(xgboost_known$order_item_id,
                       xgboost_known$return,
                       rf_known$pred,
-#                      nnet_known$return,
+                      nnet_known$return,
                       d$return)
-df_class = data.frame(xgboost_known$order_item_id,
-                      xgboost_known$return,
-                      rf_known$pred,
-                      nnet_known$return)
+df_class = data.frame(c$order_item_id,
+                      xgboost_class$return,
+                      rf_class$pred,
+                      nnet_class$return)
 
 colnames(df_known) = c("order_item_id",
                        "xgboost_return",
                        "rf_return",
-#                       "nnet_return",
+                       "nnet_return",
                        "return")
 colnames(df_class) = c("order_item_id",
                        "xgboost_return",
                        "rf_return",
                        "nnet_return")
+levels(df_known$return) = c("return0", "return1")
 
 # create cost matrix
-cost = data.frame(ifelse(d$return == 0, 0, 2.5*(3+0.1*d$item_price)),
-                  ifelse(d$return == 1, 0, 0.5 * d$item_price))
-colnames(cost) = make.names(c("X0","X1"))
+cost = data.frame(ifelse(df_known$return == 0, 0, 2.5*(3+0.1*d$item_price)),
+                  ifelse(df_known$return == 1, 0, 0.5 * d$item_price))
+colnames(cost) = levels(df_known$return)
+rownames(cost) = rownames(d)
 
 # check correlations of models
 #results <- resamples(list(mod1 = mod.rf, mod2 = mod.svm)) 
@@ -64,56 +69,45 @@ colnames(cost) = make.names(c("X0","X1"))
 # create test and training sets
 set.seed(1)
 
-idx.train = caret::createDataPartition(y = df_known$return, p = 0.75, list = FALSE) 
+idx.train = caret::createDataPartition(y = df_known$return, p = 0.8, list = FALSE) 
+df_known$return = NULL
 tr = df_known[idx.train, ]
 ts = df_known[-idx.train, ]
 
-# tune hyperparameters
-stack_params = makeParamSet(
-  makeNumericParam("cost", lower = 0, upper = 10, trafo = function(x) 2^x),
-  makeNumericParam("epsilon", lower = 0, upper = 10)
-)
-
+# train the regression
 trainTask = makeCostSensTask(data = tr,
                              cost = cost[idx.train,])
-control = makeTuneControlRandom(maxit = 50)
-resample_desc = makeResampleDesc("CV", iters = 10)
+resample_desc = makeResampleDesc("Bootstrap", iters = 100)
 
 stack_learner = makeLearner(
-  "classif.LiblineaRL2LogReg",
-  predict.type = "prob"
+  "classif.logreg",
+  predict.type = "response"
 )
 stack_learner = makeCostSensClassifWrapper(stack_learner)
-
-tuned_params = tuneParams(
-  learner = stack_learner,
-  task = trainTask,
-  resampling = resample_desc,
-  par.set = stack_params,
-  control = control)
-
-# train stacked model
-stack_tuned_learner = setHyperPars(
-  learner = stack_learner,
-  par.vals = tuned_params$x
-)
-stack_model = train(stack_learner, trainTask)
+stack_train = resample(
+                      learner    = stack_learner,
+                      task       = trainTask,
+                      resampling = resample_desc
+                      )
+stack_model = mlr::train(stack_learner, trainTask)
 
 # create final predictions
 predicted_classes = predict(stack_model, newdata = df_known)
-#predicted_class   = predict(stack_model, newdata = df_class)
+predicted_class   = predict(stack_model, newdata = df_class)
 
 # assess performance
-d.result = data.frame(d$order_item_id, ifelse(predicted_classes$data$response == "X0",0,1))
+d.result = data.frame(d$order_item_id, ifelse(predicted_classes$data$response == "return0",0,1))
 names(d.result) = c("order_item_id", "return")
-accuracy = mean(d.result[-idx.train,]$return == ts$return)
+accuracy = mean(d.result[-idx.train,]$return == d[-idx.train,]$return)
 total_accuracy = mean(d$return == d.result$return)
 
-#classdata.result = data.frame(classdata$order_item_id, predicted_class$data$response)
-#names(classdata.result) = c("order_item_id", "return")
+classdata.result = data.frame(df_class$order_item_id, ifelse(predicted_class$data$response == "return0",0,1))
+names(classdata.result) = c("order_item_id", "return")
 
 # put in NA column hack
 d.result[is.na(d$delivery_date), "return"] = 0
-#classdata.result[is.na(classdata$delivery_date), "return"] = 0
+classdata.result[is.na(c$delivery_date), "return"] = 0
 
 # and write out final predictions
+write.csv(d.result, "data/stacked_known.csv", row.names = FALSE)
+write.csv(classdata.result, "data/stacked_class.csv", row.names = FALSE)
