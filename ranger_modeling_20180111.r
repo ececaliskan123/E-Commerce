2d8fb8ca5a6c4ff15259642a12d7643a270bf910
Hyunah <bluehyunah@gmail.com>
2018-01-28 12:54:50 +0100

    I am adding updated ver

--- ranger_modeling_20180111.r
+++ ranger_modeling_20180111.r
@@ -1,6 +1,4 @@
-#### Ece's data cleaning base
-## row 99 in Known.r file, retail$return2 <- retail$return 
-## I need 0,1 so I add row 99 because ece switched return class like retail$return <- ("keep","return")
+source('load_data.R')
 
 ### Random forest
 library(ranger)
@@ -8,29 +6,21 @@ library(dplyr)
 library(caret)
 # From Parameter tuning file
 set.seed(124)
-n <- nrow(retail) 
+n <- nrow(df_known) 
 sample.size <- ceiling(n*0.8)
-idx.train <- createDataPartition(y =retail$return2, p = 0.8, list = FALSE) 
-tr <- retail[idx.train, ]  # training set
-ts <- retail[-idx.train, ] # test set 
+idx.train <- createDataPartition(y =df_known$return, p = 0.8, list = FALSE) 
+tr <- df_known[idx.train, ]  # training set
+ts <- df_known[-idx.train, ] # test set 
 # The return column
-return <- "return2"
+return <- "return"
 # The input variables
-vars <- c("user_dob", "delivery_duration", "user_maturity",
-          "user_state", "item_price", "item_color", "item_size", "month_of_delivery")
+vars <- c("user_id", "order_item_id", "order_date","month_of_delivery", "price_and_age",
+          "item_id", "item_size", "brand_id", "item_price", "user_dob", "user_reg_date", "delivery_duration")
 # Create the formula string for returns as a function of the inputs
 fmla <- paste(return, "~", paste(vars, collapse = " + "))
 # Fit and print the random forest model
-# Hyperparameters with CV
-(retail_model_rf <- ranger(fmla, # formula
-                           tr,  #data
-                           num.trees = 200,
-                           mtry = 6,
-                           num.threads = 1,
-                           verbose = FALSE,
-                           respect.unordered.factors = TRUE))
-# Hyperparameters with bootstrap 632, 10 folds CV
-(retail_model_rf2 <- ranger(fmla, # formula
+# Hyperparameters with 5 folds CV
+(df_known_rfmodel_CV <- ranger(fmla, # formula
                            tr,  #data
                            num.trees = 800,
                            mtry = 6,
@@ -38,90 +28,98 @@ fmla <- paste(return, "~", paste(vars, collapse = " + "))
                            verbose = FALSE,
                            respect.unordered.factors = TRUE))
 # Make predictions on the ts data
-ts$pred <- predict(retail_model_rf, ts)$predictions
-ts$pred2 <- predict(retail_model_rf2, ts)$predictions
+ts$pred <- predict(df_known_rfmodel_CV, ts)$predictions
 # Calculate the RMSE of the predictions
 ts %>% 
-  mutate(residual = return2 - pred)  %>%        # calculate the residual
+  mutate(residual = return - pred)  %>%        # calculate the residual
   summarize(rmse  = sqrt(mean(residual^2)))    # calculate rmse
 # Plot actual outcome vs predictions (predictions on x-axis)
-ggplot(ts, aes(x = pred, y = return2)) + 
+ggplot(ts, aes(x = pred, y = return)) + 
   geom_point() + 
   stat_smooth(method = 'glm', method.args = list(family = 'binomial'), se = FALSE) # Smoothed curve w/o standard errors
-#Prediction for retail
-retail$pred <- predict(retail_model_rf, retail)$predictions
-retail$pred2 <- predict(retail_model_rf2, retail)$predictions
-retail1 <- subset(retail, select = c(order_item_id, pred))
-retail2 <- subset(retail, select = c(order_item_id, pred2))
-csv <- write_csv(retail1, 'randomforest_probability_retail_5CV.csv')
-csv1 <- write_csv(retail2, 'randomforest_probability_retail_bootstrap632.csv')
-
+#Prediction for df_known
+df_known$pred <- predict(df_known_rfmodel_CV, df_known)$predictions
+df_known1 <- subset(df_known, select = c(order_item_id, pred))
+csv <- write_csv(df_known1, 'randomforest_known.csv')
 
 # Saving optimal rf model
-saveRDS(retail_model_rf2, file = "models/RF_Model_Par.R") 
+saveRDS(df_known_rfmodel_CV, file = "models/RF_Model_Par.R") 
 
-
-# Accuracy
-prob.pred_known  = as.vector(retail$pred)
+# Accuracy : 0.9256  
+prob.pred_known  = as.vector(df_known$pred)
 class.pred_known  = ifelse(prob.pred_known > 0.5, "1", "0")
-confusionMatrix(data = class.pred_known, reference = retail$return2, positive = "1")
+confusionMatrix(data = class.pred_known, reference = df_known$return, positive = "1")
 
-prob.pred_known  = as.vector(retail$pred2)
-class.pred_known  = ifelse(prob.pred_known > 0.5, "1", "0")
-confusionMatrix(data = class.pred_known, reference = retail$return2, positive = "1")
 
-retail$pred_class = ifelse(prob.pred_known > 0.5, "1", "0")
+
 
 #### Prediction
 library(readr)
-sales$pred <- predict(retail_model_rf, sales)$predictions
-sales$pred_return <- with(sales, ifelse(pred < 0.5, 0, 1))
-table(sales$pred_return)
+df_class$pred <- predict(df_known_rfmodel_CV, df_class)$predictions
+df_class$pred_return <- with(df_class, ifelse(pred < 0.5, 0, 1))
+table(df_class$pred_return)
 
-
-# Compare the histograms between the predicted values(Probabilities) of ts(known) and sales(class) 
+# Compare the histograms between the predicted values(Probabilities) of ts(known) and df_class(class) 
 par(mfrow = c(1, 2)) # 1 row, 2 columns
 hist(ts$pred, main = 'Distribution of Predicted Probabilities for Known Data', xlab = 'Predicted Probabilities (Known Data)')
-hist(sales$pred, main = 'Distribution of Predicted Probabilities for Class Data', xlab = 'Predicted Probabilities (Class Data)')
-#Select only 2 columns for csv files(probability, class)
-sales1 <- subset(sales, select = c(order_item_id, pred))
-csv <- write_csv(sales1, 'randomforest_probability.csv')
-sales2 <- subset(sales, select = c(order_item_id, pred_return))
-csv <- write_csv(sales2, 'randomforest_class.csv')
+hist(df_class$pred, main = 'Distribution of Predicted Probabilities for Class Data', xlab = 'Predicted Probabilities (Class Data)')
 
-#### Parameter tuning with 'mlr'
-# Cross validation with 'mlr'
+#Select only 2 columns for csv files(probability, class)
+df_class1 <- subset(df_class, select = c(order_item_id, pred))
+csv1 <- write_csv(df_class1, 'randomforest_class_probability.csv')
+df_class2 <- subset(df_class, select = c(order_item_id, pred_return))
+csv2 <- write_csv(df_class2, 'randomforest_class.csv')
+
+
+
+
+#### Parameter tuning for ranger(randomforest) with mlr 
 library(mlr)
 library(tidyr)
 library(ranger)
 
-retail2 <- retail
-retail2 <- subset(retail2, select = -c(delivery_date))
-retail2 <- separate(retail2, col = order_date , into = c("order_year", "order_month", "order_day"), sep="-") 
-retail2 <- separate(retail2, col = user_reg_date, into = c("user_reg_year", "user_reg_month", "user_reg_day"), sep="-")
+amend_features = function(dd){
+  dd = subset(dd, select = -c(delivery_date))
+  dd = subset(dd, select = -c(user_dob, user_maturity, user_title, user_state, item_color))
+  
+  dd$order_year  = as.numeric(format(dd$order_date, "%Y"))
+  dd$order_month = as.numeric(format(dd$order_date, "%m"))
+  dd$order_day   = as.numeric(format(dd$order_date, "%d"))
+  dd             = subset(dd, select=-order_date)
+  
+  dd$reg_year  = as.numeric(format(dd$user_reg_date, "%Y"))
+  dd$reg_month = as.numeric(format(dd$user_reg_date, "%m"))
+  dd$reg_day   = as.numeric(format(dd$user_reg_date, "%d"))
+  dd           = subset(dd, select=-user_reg_date)
+  
+  if("return" %in% colnames(dd)) {
+    dd = normalizeFeatures(dd, target="return")
+    dd = createDummyFeatures(dd, target="return", cols=c("item_size"))
+  } else {
+    dd = createDummyFeatures(dd, cols=c("item_size"))
+  }
+  
+  return(dd)
+}
 
-head(retail2)
+#setwd("/mnt/learning/business-analytics-data-science/groupwork/")
+source('load_data.R')
+#d = read_and_preprocess_data_file('data/BADS_WS1718_known.csv')
+#classdata = read_and_preprocess_data_file('data/BADS_WS1718_class.csv')
 
-retail2$order_year <- as.numeric(retail2$order_year)
-retail2$order_month <- as.numeric(retail2$order_month)
-retail2$order_day <- as.numeric(retail2$order_day)
-retail2$user_reg_year <- as.numeric(retail2$user_reg_year)
-retail2$user_reg_month <- as.numeric(retail2$user_reg_month)
-retail2$user_reg_day <- as.numeric(retail2$user_reg_day)
-retail2$month_of_delivery <- as.numeric(retail2$month_of_delivery)
-
-retail2 <- createDummyFeatures(retail2, target = "return2")
+### TODO AMEND BLOCK AFTER FEATURE ENGINEERING
+retail2 = amend_features(df_known)
 
 # Splitting the data into a test and a training set 
 set.seed(124)
-n <- nrow(retail) 
+n <- nrow(retail2) 
 sample.size <- ceiling(n*0.8)
-idx.train2 <- createDataPartition(y =retail2$return2, p = 0.75, list = FALSE) 
-tr1 <- retail3[idx.train, ]  # training set
-ts1 <- retail3[-idx.train, ] # test set 
+idx.train <- createDataPartition(y =retail2$return, p = 0.8, list = FALSE) 
+tr1 <- retail2[idx.train, ]  # training set
+ts1 <- retail2[-idx.train, ] # test set 
 
 # Define task and learner
-task <- makeClassifTask(data = tr1, target = "return2", positive = "1")
+task <- makeClassifTask(data = tr1, target = "return", positive = "1")
 # Set up a structure to save the expected results
 # A number of models
 modelLib <- list()
@@ -130,7 +128,7 @@ yhat <- list()
 # AUC performance for each model
 auc <- list()
 
-rf <- makeLearner("classif.ranger", predict.type="prob", predict.threshold = 0.6)
+rf <- makeLearner("classif.ranger", predict.type="prob")
 rf
 ## Tuning
 # Hyperparameter setting
@@ -143,17 +141,12 @@ rf.parms <- makeParamSet(
 # How dense should the parameters be selected from the ranges?
 tuneControl <- makeTuneControlGrid(resolution = 3, tune.threshold = FALSE)
 # Sampling strategy
-# Given our small dataset, we do 10-fold cross-validation
-rdesc <- makeResampleDesc(method = "CV", iters = 10, stratify = TRUE)
-rdesc_632 <- makeResampleDesc(method = "Bootstrap", iters = 10, stratify = TRUE, predict = "both")
+# Given our small dataset, we do 5-fold cross-validation
+rdesc <- makeResampleDesc(method = "CV", iters = 5, stratify = TRUE)
 # Start tuning with the defined options
 timing <- list()
 timing[["simple"]] <- system.time(
   tuning <- tuneParams(rf, task = task, resampling = rdesc,
-                       par.set = rf.parms, control = tuneControl, measures = mlr::auc)
-)
-timing[["simple"]] <- system.time(
-  tuning <- tuneParams(rf, task = task, resampling = rdesc_632,
                        par.set = rf.parms, control = tuneControl, measures = mlr::auc)
 )
 # Extract optimal parameter values after tuning 
@@ -175,7 +168,10 @@ yhat[["rf"]] <- predict(modelLib[["rf"]], newdata = ts1)
 str(yhat[["rf"]])
 # Calculate AUC performance on test set 
 auc[["rf"]] <- mlr::performance(yhat[["rf"]], measures = mlr::auc)
-auc[["rf"]] 
+auc[["rf"]] #0.9118319 
+
+
+
 
 
 
