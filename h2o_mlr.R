907f03d4147632ba9b76db0d770cb107f1d3da92
Felix Wolff <felix.wolff2@student.hpi.de>
2018-02-01 01:16:11 +0100

    Updated h2o deeplearning with new helper, features, variable importance and PDPs

--- h2o_mlr.R
+++ h2o_mlr.R
@@ -2,39 +2,13 @@
 
 if(!require("h2o")) install.packages("h2o"); library("h2o")
 if(!require("mlr")) install.packages("mlr"); library("mlr")
+if(!require("pdp")) install.packages("pdp"); library("pdp")
 if(!require("parallelMap")) install.packages("parallelMap"); library("parallelMap")
 if(!require("data.table")) install.packages("data.table"); library("data.table")
-
-amend_features = function(dd){
-  dd = subset(dd, select = -c(delivery_date))
-  dd = subset(dd, select = -c(user_dob, user_maturity, user_title, user_state, item_color, item_price, item_size))
-  # Added the feature item_price and item_size to the previous line due to low RF variable importance scores
-  dd$order_year  = as.numeric(format(dd$order_date, "%Y"))
-  dd$order_month = as.numeric(format(dd$order_date, "%m"))
-  dd$order_day   = as.numeric(format(dd$order_date, "%d"))
-  dd             = subset(dd, select=-order_date)
-  
-  dd$reg_year  = as.numeric(format(dd$user_reg_date, "%Y"))
-  dd$reg_month = as.numeric(format(dd$user_reg_date, "%m"))
-  dd$reg_day   = as.numeric(format(dd$user_reg_date, "%d"))
-  dd           = subset(dd, select=-user_reg_date)
-  
-  if("return" %in% colnames(dd)) {
-    dd = normalizeFeatures(dd, target="return")
-    # dd = createDummyFeatures(dd, target="return", cols=c("item_size"))
-  } else {
-    # dd = createDummyFeatures(dd, cols=c("item_size"))
-  }
-  # Deselected createDummyFeatures due to the deletion of fetures item_size and item_price (see above)
-  return(dd)
-}
-
-# setwd("/mnt/learning/business-analytics-data-science/groupwork/")
+# load helper to preprocess data and select fetures
+source('helpers/amend_features.R')
+# load data
 source('load_data.R')
-#d = read_and_preprocess_data_file('data/BADS_WS1718_known.csv')
-#classdata = read_and_preprocess_data_file('data/BADS_WS1718_class.csv')
-
-### TODO AMEND BLOCK AFTER FEATURE ENGINEERING
 dn = amend_features(df_known)
 classdatan = amend_features(df_class)
 ###############################################
@@ -134,3 +108,13 @@ classdata.result[is.na(df_class$delivery_date), "return"] = 0
 write.csv(d.result, "data/h2o_known.csv", row.names = FALSE)
 write.csv(classdata.result, "data/h2o_class.csv", row.names = FALSE)
 
+## Variable importance using the olden method
+h2o_imp_df <- h2o.varimp(h2o_model)
+save(h2o_imp_df, file = "data/h2o_imp_df")
+
+h2o_imp_plot <- h2o.varimp_plot(h2o_model, num_of_features = 17)
+save(h2o_imp_plot, file = "data/h2o_imp_plot")
+
+## Calculate and plot PDPs for all variables
+partialPlots.h2o <- h2o.predict(h2o_model, data.pdp)
+###
