c12fe056ee70093e917ed2494177437bace48263
Felix Wolff <felix.wolff2@student.hpi.de>
2018-02-01 09:42:07 +0100

    Retrain xgboost with new feature array

--- xgboost_model_construction.R
+++ xgboost_model_construction.R
@@ -5,39 +5,9 @@ if(!require("xgboost")) install.packages("xgboost"); library("xgboost")
 if(!require("mlr")) install.packages("mlr"); library("mlr")
 if(!require("tidyverse")) install.packages("tidyverse"); library("tidyverse")
 
-amend_features = function(dd){
-  dd = subset(dd, select = -c(user_dob,
-                              user_maturity,
-                              user_title,
-                              user_state,
-                              item_color,
-                              delivery_date,
-                              item_size))
-  
-  dd$order_year  = as.numeric(format(dd$order_date, "%Y"))
-  dd$order_month = as.numeric(format(dd$order_date, "%m"))
-  dd$order_day   = as.numeric(format(dd$order_date, "%d"))
-  dd             = subset(dd, select=-order_date)
-  
-  dd$reg_year  = as.numeric(format(dd$user_reg_date, "%Y"))
-  dd$reg_month = as.numeric(format(dd$user_reg_date, "%m"))
-  dd$reg_day   = as.numeric(format(dd$user_reg_date, "%d"))
-  dd           = subset(dd, select=-user_reg_date)
-  
-  if("return" %in% colnames(dd)) {
-    dd = mlr::normalizeFeatures(dd, target="return")
-    #dd = createDummyFeatures(dd, target="return", cols=c("item_size"))
-  } else {
-    #dd = createDummyFeatures(dd, cols=c("item_size"))
-  }
-
-  return(dd)
-}
-
 #setwd("/mnt/learning/business-analytics-data-science/groupwork/")
 source('load_data.R')
-#d = read_and_preprocess_data_file('data/BADS_WS1718_known.csv')
-#classdata = read_and_preprocess_data_file('data/BADS_WS1718_class.csv')
+source('helpers/amend_features.R')
 
 ### TODO AMEND BLOCK AFTER FEATURE ENGINEERING
 dn = amend_features(df_known)
@@ -78,7 +48,7 @@ xgb_learner = makeLearner(
     objective = "binary:logistic",
     eval_metric = "error",
     nrounds = 200,
-    base_score = mean(tr$return)
+    base_score = mean(tr$return == 1)
   )
 )
 
@@ -112,7 +82,6 @@ tr_accuracy     = mean(ifelse(d.result[idx.train, "return"] > 0.5, 1,0) == df_kn
 classdata.result = data.frame(df_class$order_item_id, predicted_class$data$prob.1)
 names(classdata.result) = c("order_item_id", "return")
 
-# TODO put in known class hack
 d.result[is.na(df_known$delivery_date), "return"] = 0
 classdata.result[is.na(df_class$delivery_date), "return"] = 0
 
@@ -120,12 +89,7 @@ save(xgb_model, file = "models/xgboost_mlr.model")
 write.csv(d.result, "data/xgboost_known.csv", row.names = FALSE)
 write.csv(classdata.result, "data/xgboost_class.csv", row.names = FALSE)
 
-if (FALSE) {
-  importance = xgb.importance(feature_names = colnames(tr), model = xgb_model$learner.model)
-  s_cols = importance[,c("Gain","Cover","Frequency")]
-  importance = data.frame(importance[,"Feature"], s_cols)
-  head(importance)
-  
-  pd.xgboost = generatePartialDependenceData(xgb_model, trainTask, "item_price")
-  plotPartialDependence(pd.xgboost)
-}
+importance = xgb.importance(feature_names = colnames(tr), model = xgb_model$learner.model)
+s_cols = importance[,c("Gain","Cover","Frequency")]
+importance = data.frame(importance[,"Feature"], s_cols)
+write.csv(importance, "data/xgb_importance.csv", row.names = F)
