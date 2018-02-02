c6241f06bb0b18d21cd01b963e7e4f7ea01667eb
Felix Wolff <felix.wolff2@student.hpi.de>
2018-02-02 19:40:57 +0100

    Ranger code

--- old/ranger_model_construction.R
+++ old/ranger_model_construction.R
@@ -1,21 +1,46 @@
-source('load_data.R')
+### random forest with ranger and caret
 
-### Random forest
-library(ranger)
-library(dplyr)
-library(caret)
-# From Parameter tuning file
-n <- nrow(df_known) 
-sample.size <- ceiling(n*0.8)
+if(!require("ranger")) install.packages("ranger"); library("ranger")
+if(!require("caret")) install.packages("caret"); library("caret")
+if(!require("dplyr")) install.packages("dplyr"); library("dplyr")
+
+# load helper to preprocess data and select fetures
+source('helpers/amend_features.R')
+# load data
+source('load_data.R')
+dn = amend_features(df_known)
+dn[is.na(dn)] = 0
+classdatan = amend_features(df_class)
+
+###############################################
 set.seed(1)
-idx.train <- createDataPartition(y =df_known$return, p = 0.8, list = FALSE) 
-tr <- df_known[idx.train, ]  # training set
-ts <- df_known[-idx.train, ] # test set 
+
+idx.train = caret::createDataPartition(y = dn$return, p = 0.8, list = FALSE) 
+tr = dn[idx.train, ]
+ts = dn[-idx.train, ]
+
+# From Parameter tuning file
+n <- nrow(dn) 
 # The return column
 return <- "return"
 # The input variables
-vars <- c("user_id", "order_item_id", "order_date","month_of_delivery", "price_and_age",
-          "item_id", "item_size", "brand_id", "item_price", "user_dob", "user_reg_date", "delivery_duration")
+vars <- c("user_id", 
+          "order_item_id", 
+          "item_id", 
+          "item_size", 
+          "item_color", 
+          "brand_id", 
+          "user_dob", 
+          "delivery_duration", 
+          "user_maturity", 
+          "price_and_age", 
+          "avg_return", 
+          "nr_obs", 
+          "order_month", 
+          "order_day", 
+          "del_year", 
+          "del_month", 
+          "del_day")
 # Create the formula string for returns as a function of the inputs
 fmla <- paste(return, "~", paste(vars, collapse = " + "))
 # Fit and print the random forest model
@@ -26,104 +51,65 @@ fmla <- paste(return, "~", paste(vars, collapse = " + "))
                            mtry = 6,
                            num.threads = 1,
                            verbose = FALSE,
-                           respect.unordered.factors = TRUE))
+                           respect.unordered.factors = TRUE
+                          ))
 # Make predictions on the ts data
 ts$pred <- predict(df_known_rfmodel_CV, ts)$predictions
-# Calculate the RMSE of the predictions
-ts %>% 
-  mutate(residual = return - pred)  %>%        # calculate the residual
-  summarize(rmse  = sqrt(mean(residual^2)))    # calculate rmse
-# Plot actual outcome vs predictions (predictions on x-axis)
-ggplot(ts, aes(x = pred, y = return)) + 
-  geom_point() + 
-  stat_smooth(method = 'glm', method.args = list(family = 'binomial'), se = FALSE) # Smoothed curve w/o standard errors
-#Prediction for df_known
-df_known$pred <- predict(df_known_rfmodel_CV, df_known)$predictions
-a <- which(is.na(df_known$delivery_date))
-df_known$pred[a] <- 0 
-df_known1 <- subset(df_known, select = c(order_item_id, pred))
-csv <- write_csv(df_known1, 'randomforest_known.csv')
-
+#Prediction for dn1
+dn$pred <- predict(df_known_rfmodel_CV, dn)$predictions
+a <- which(is.na(dn$delivery_date))
+dn$pred[a] <- 0 
+dn <- subset(dn, select = c(order_item_id, pred))
+dn$order_item_id <- df_known$order_item_id
+csv <- write.csv(dn, 'old/new_ranger_known.csv', row.names = FALSE)
 # Saving optimal rf model
-saveRDS(df_known_rfmodel_CV, file = "models/RF_Model_Par.R") 
+save(df_known_rfmodel_CV, file = "old/rf_ranger.model") 
 
-# Accuracy : 0.9256
-prob.pred_known  = as.vector(df_known$pred)
+# Accuracy : 96.38%
+prob.pred_known  = as.vector(dn$pred)
 class.pred_known  = ifelse(prob.pred_known > 0.5, "1", "0")
 confusionMatrix(data = class.pred_known, reference = df_known$return, positive = "1")
 
-
-
-
 #### Prediction
 library(readr)
-df_class$pred <- predict(df_known_rfmodel_CV, df_class)$predictions
+df_class_amended<-amend_features(df_class)
+df_class_amended[is.na(df_class_amended)] = 0
+df_class_amended$pred <- predict(df_known_rfmodel_CV, df_class_amended)$predictions
 b <- which(is.na(df_class$delivery_date))
-df_class$pred[b] <- 0 
-df_class$pred_return <- with(df_class, ifelse(pred < 0.5, 0, 1))
-table(df_class$pred_return)
+df_class_amended$pred[b] <- 0
+df_class_amended$pred <- as.numeric(as.character(df_class_amended$pred))
+df_class_amended$return <- ifelse(df_class_amended$pred > 0.5, 1, 0)
+table(df_class_amended$return)
+# Select only 2 columns for csv files(probability, class)
+df_class_amended1 <- subset(df_class_amended, select = c(order_item_id, pred))
+csv1 <- write.csv(df_class_amended1, "old/ranger_class_probability.csv")
+df_class_amended2 <- subset(df_class_amended, select = c(order_item_id, return))
+csv2 <- write.csv(df_class_amended2, "old/ranger_class.csv")
 
-# Compare the histograms between the predicted values(Probabilities) of ts(known) and df_class(class) 
-par(mfrow = c(1, 2)) # 1 row, 2 columns
-hist(ts$pred, main = 'Distribution of Predicted Probabilities for Known Data', xlab = 'Predicted Probabilities (Known Data)')
-hist(df_class$pred, main = 'Distribution of Predicted Probabilities for Class Data', xlab = 'Predicted Probabilities (Class Data)')
-
-#Select only 2 columns for csv files(probability, class)
-df_class1 <- subset(df_class, select = c(order_item_id, pred))
-csv1 <- write_csv(df_class1, 'randomforest_class_probability.csv')
-df_class2 <- subset(df_class, select = c(order_item_id, pred_return))
-csv2 <- write_csv(df_class2, 'randomforest_class.csv')
-
-
-
+################################################
 
 #### Parameter tuning for ranger(randomforest) with mlr 
 library(mlr)
 library(tidyr)
 library(ranger)
 
-amend_features = function(dd){
-  dd = subset(dd, select = -c(delivery_date))
-  dd = subset(dd, select = -c(user_dob, user_maturity, user_title, user_state, item_color))
-  
-  dd$order_year  = as.numeric(format(dd$order_date, "%Y"))
-  dd$order_month = as.numeric(format(dd$order_date, "%m"))
-  dd$order_day   = as.numeric(format(dd$order_date, "%d"))
-  dd             = subset(dd, select=-order_date)
-  
-  dd$reg_year  = as.numeric(format(dd$user_reg_date, "%Y"))
-  dd$reg_month = as.numeric(format(dd$user_reg_date, "%m"))
-  dd$reg_day   = as.numeric(format(dd$user_reg_date, "%d"))
-  dd           = subset(dd, select=-user_reg_date)
-  
-  if("return" %in% colnames(dd)) {
-    dd = normalizeFeatures(dd, target="return")
-    dd = createDummyFeatures(dd, target="return", cols=c("item_size"))
-  } else {
-    dd = createDummyFeatures(dd, cols=c("item_size"))
-  }
-  
-  return(dd)
-}
-
-#setwd("/mnt/learning/business-analytics-data-science/groupwork/")
+# load helper to preprocess data and select fetures
+source('helpers/amend_features.R')
+# load data
 source('load_data.R')
-#d = read_and_preprocess_data_file('data/BADS_WS1718_known.csv')
-#classdata = read_and_preprocess_data_file('data/BADS_WS1718_class.csv')
+dn = amend_features(df_known)
+dn[is.na(dn)] = 0
+classdatan = amend_features(df_class)
 
-### TODO AMEND BLOCK AFTER FEATURE ENGINEERING
-retail2 = amend_features(df_known)
-
-# Splitting the data into a test and a training set 
+###############################################
 set.seed(1)
-n <- nrow(retail2) 
-sample.size <- ceiling(n*0.8)
-idx.train <- createDataPartition(y =retail2$return, p = 0.8, list = FALSE) 
-tr1 <- retail2[idx.train, ]  # training set
-ts1 <- retail2[-idx.train, ] # test set 
+
+idx.train = caret::createDataPartition(y = dn$return, p = 0.8, list = FALSE) 
+tr = dn[idx.train, ]
+ts = dn[-idx.train, ]
 
 # Define task and learner
-task <- makeClassifTask(data = tr1, target = "return", positive = "1")
+task <- makeClassifTask(data = tr, target = "return", positive = "1")
 # Set up a structure to save the expected results
 # A number of models
 modelLib <- list()
@@ -143,7 +129,7 @@ rf.parms <- makeParamSet(
   makeIntegerParam("num.trees", 200, 800) # Number of tree, smaller -> faster
 ) 
 # How dense should the parameters be selected from the ranges?
-tuneControl <- makeTuneControlGrid(resolution = 3, tune.threshold = FALSE)
+tuneControl <- makeTuneControlGrid(resolution = 3, tune.threshold = TRUE)
 # Sampling strategy
 # Given our small dataset, we do 5-fold cross-validation
 rdesc <- makeResampleDesc(method = "CV", iters = 5, stratify = TRUE)
