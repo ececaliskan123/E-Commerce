f27d295234a84fdb09c1df02c4c2307c101703ee
tengallonheadNS <heer0@hotmail.de>
2018-02-03 13:36:50 +0100

    New ranger code with current feature selection

--- old/ranger_tuning.R
+++ old/ranger_tuning.R
@@ -1,7 +1,8 @@
-### Parameter tuning for ranger(randomforest) with mlr 
-library(mlr)
-library(tidyr)
-library(ranger)
+### Parameter tuning for ranger (random forest) with mlr 
+
+if(!require("ranger")) install.packages("ranger"); library("ranger")
+if(!require("mlr")) install.packages("mlr"); library("mlr")
+if(!require("parallelMap")) install.packages("parallelMap"); library("parallelMap")
 
 # load helper to preprocess data and select fetures
 source('helpers/amend_features.R')
@@ -19,7 +20,8 @@ tr = dn[idx.train, ]
 ts = dn[-idx.train, ]
 
 # Define task and learner
-task <- makeClassifTask(data = tr, target = "return", positive = "1")
+trainTask <- makeClassifTask(data = tr, target = "return", positive = "1")
+testTask <- makeClassifTask(data = ts, target = "return", positive = "1")
 # Set up a structure to save the expected results
 # A number of models
 modelLib <- list()
@@ -36,19 +38,23 @@ rf
 rf.parms <- makeParamSet(
   # The recommendation for mtry by Breiman is squareroot number of columns
   makeIntegerParam("mtry", lower = 2, upper = 6), # Number of features selected at each node, smaller -> faster
-  makeIntegerParam("num.trees", 200, 800) # Number of tree, smaller -> faster
+  makeIntegerParam("num.trees", lower = 200, upper = 1000) # Number of tree, smaller -> faster
 ) 
 # How dense should the parameters be selected from the ranges?
-tuneControl <- makeTuneControlGrid(resolution = 3, tune.threshold = TRUE)
+tuneControl <- makeTuneControlRandom(maxit = 50L, tune.threshold = TRUE)
 # Sampling strategy
 # Given our small dataset, we do 5-fold cross-validation
 rdesc <- makeResampleDesc(method = "CV", iters = 5, stratify = TRUE)
+# Activate parallel computing with all cores
+parallelStartSocket(parallel::detectCores())
 # Start tuning with the defined options
 timing <- list()
 timing[["simple"]] <- system.time(
-  tuning <- tuneParams(rf, task = task, resampling = rdesc,
+  tuning <- tuneParams(learner = rf, task = trainTask, resampling = rdesc,
                        par.set = rf.parms, control = tuneControl, measures = mlr::auc)
 )
+# Stop parallelization
+parallelStop()
 # Extract optimal parameter values after tuning 
 tuning$x
 # Investigate the results in detail
@@ -61,11 +67,51 @@ rf
 rf_tuned <- setHyperPars(rf, par.vals = tuning$x)
 rf_tuned #Hyperparameters: num.threads=1,verbose=FALSE,respect.unordered.factors=TRUE,mtry=6,num.trees=200
 # Train the model on the full training data (not only a CV-fold)
-modelLib[["rf"]] <- mlr::train(rf_tuned, task = task)
-print(modelLib[["rf"]])
-# Make prediction on test data
-yhat[["rf"]] <- predict(modelLib[["rf"]], newdata = ts1)
-str(yhat[["rf"]])
+ranger_model <- mlr::train(rf_tuned, task = trainTask)
+# Save model
+save(ranger_model, file = "old/ranger_mlr.model")
+
+# Predict on test set and assess performance based on auc and acc
+yhat[["ranger_prob_test"]]<-predict(ranger_model, testTask)
+yhat[["ranger_class_test"]]<-yhat[["ranger_prob_test"]]$data$response
+auc[["ranger_test"]]<-mlr::performance(yhat[["ranger_prob_test"]], measures = mlr::auc)
+auc[["ranger_test"]]
+acc[["ranger_test"]]<-mean(yhat[["ranger_class_test"]] == yhat[["ranger_prob_test"]]$data$truth)
+acc[["ranger_test"]]
+
+# Use tuned model to predict whole known dataset
+yhat[["ranger_known"]]<-predict(ranger_model, newdata = dn)
+
+# Use tuned model to predict whole class dataset
+yhat[["ranger_class"]]<-predict(ranger_model, newdata = classdatan)
+
+# Create an object containing order_item_id and predicted probabilities for known dataset
+d.result<-data.frame(df_known$order_item_id, yhat[["ranger_known"]]$data$prob.1)
+names(d.result)<-c("order_item_id", "return")
+
+# Assess total accuracy on known dataset 
+acc[["ranger_total"]]<-mean(ifelse(d.result$return > yhat[["ranger_known"]]$threshold[1], 1, 0) == df_known$return)
+acc[["ranger_total"]]
+
+# Create an object containing order_item_id and predicted probabilities for class dataset
+classdata.result = data.frame(df_class$order_item_id, yhat[["ranger_class"]]$data$prob.1)
+names(classdata.result) = c("order_item_id", "return")
+
+# TODO put in known class hack
+d.result[is.na(df_known$delivery_date), "return"] = 0
+classdata.result[is.na(df_class$delivery_date), "return"] = 0
+
+write.csv(d.result, "old/ranger_known.csv", row.names = FALSE)
+write.csv(classdata.result, "old/ranger_class.csv", row.names = FALSE)
+
+
+
+
+
+
+
+
+
 # Calculate AUC performance on test set 
 auc[["rf"]] <- mlr::performance(yhat[["rf"]], measures = mlr::auc)
 auc[["rf"]] #0.91037 
