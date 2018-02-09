842a73262fe7e40cdea76c8de4e5832e5bc6a140
tengallonheadNS <heer0@hotmail.de>
2018-02-09 21:20:29 +0100

    corr table helper

--- helpers/amend_features_beta.R
+++ helpers/amend_features_beta.R
@@ -9,9 +9,9 @@ amend_features_beta = function(dd){
   dd$del_month = as.numeric(format(dd$delivery_date, "%m"))
   dd$del_day   = as.numeric(format(dd$delivery_date, "%d"))
   
-  dn_na$reg_year = as.numeric(format(dn_na$user_reg_date, "%Y"))
-  dn_na$reg_month = as.numeric(format(dn_na$user_reg_date, "%m"))
-  dn_na$reg_day   = as.numeric(format(dn_na$user_reg_date, "%d"))
+  dd$reg_year = as.numeric(format(dn_na$user_reg_date, "%Y"))
+  dd$reg_month = as.numeric(format(dn_na$user_reg_date, "%m"))
+  dd$reg_day   = as.numeric(format(dn_na$user_reg_date, "%d"))
   
   if("return" %in% colnames(dd)) {
     dd = normalizeFeatures(dd, target="return")
